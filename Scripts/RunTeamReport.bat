cd9f771f3db3255c47624b5bcb86b0e1c53a707d
Seamus Mackie <seamus.mackie@verint.com>
2018-03-28 14:33:26 +0100

    Updated bat locaitons

--- Scripts/RunTeamReport.bat
+++ Scripts/RunTeamReport.bat
@@ -1,2 +1,2 @@
 @ECHO OFF
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& 'C:\xampp\htdocs\Coyote\Scripts\RunReport.ps1' -teamname %1 -reportname %2 -buildno %3";
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& 'C:\Coyote\Scripts\RunReport.ps1' -teamname %1 -reportname %2 -buildno %3";
--- Scripts/dltestres.bat
+++ Scripts/dltestres.bat
@@ -1,2 +1,2 @@
 @ECHO OFF
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& 'C:\xampp\htdocs\Coyote\Scripts\dltestres.ps1' -buildno %1";
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& 'C:\Coyote\Scripts\dltestres.ps1' -buildno %1";
