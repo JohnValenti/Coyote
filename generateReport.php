a2c819c7334aaab8483cf92d3a4d430f5d357936
Seamus Mackie <seamus.mackie@verint.com>
2018-03-28 13:47:57 +0100

    Clean up

---
+++ generateReport.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <body>
+
+        <h1>Jacoco Report Generated</h1>
+    
+        <?php
+            $buildno = $_GET['buildno'];
+            $teamname = $_GET['teamname'];
+            $version = $_GET['version'];
+            $pipeline = $_GET['pipeline'];
+        
+            ini_set('max_execution_time', 300); //300 seconds = 5 minutes
+            
+            // download test resources
+            exec('C:\xampp\htdocs\Coyote\Scripts\dltestres.bat ' .$buildno.'');
+        
+            // download team exec
+            exec('C:\xampp\htdocs\Coyote\Scripts\dlteamrep.bat ' .$teamname.' '.$version.' '.$pipeline.' '.$buildno.'');
+                   
+            //run ant command, move generated report and clean up
+            exec('C:\xampp\htdocs\Coyote\Scripts\RunTeamReport.bat ' .$teamname.' '.$pipeline.' '.$buildno.'');
+        ?>
+    </body>
+</html>
